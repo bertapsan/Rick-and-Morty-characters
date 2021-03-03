8a253224b256b21cdb70c19e1cc965dc581c54ba
bertapsan <bertapsan@gmail.com>
2021-03-03 17:09:05 +0100

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                Hola Mundo
+            </div>
+        );
+    }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/App.css';
+import './stylesheets/index.css';
+import App from './components/App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
